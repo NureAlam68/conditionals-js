42a9d5b47d6073823709e67be6a81c42f0b51d85
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 13:28:13 +0600

    adde task four

---
+++ taskfour.js
@@ -0,0 +1,35 @@
+let myMarks = 81;
+let friendMarks = 90;
+
+if(myMarks > 80 && friendMarks > 80) {
+    console.log('Go for a lunch')
+} 
+
+
+else if(myMarks > 80 && friendMarks >= 60) {  
+        console.log('Good luck next time')
+}  
+
+else if(myMarks > 80 && friendMarks >= 40) {
+        console.log('Keep your friend message unseen')
+}
+
+else if(myMarks > 80 && friendMarks < 40) {
+        console.log('block your friend')
+}
+
+else{
+    console.log('Go to home and sleep and act sad')
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- taskthree.js
+++ taskthree.js
@@ -1,26 +1,26 @@
-let marks = 87;
+let marks = 59;
 
 if(marks < 100 && marks >= 90){
-    console.log('A');
+    console.log('Your Grade: A');
 }
 
 else {
     if(marks < 89 && marks >= 80){
-        console.log('B');
+        console.log('Your Grade: B');
     }
 
     else {
         if(marks < 79 && marks >= 70){
-            console.log('C');
+            console.log('Your Grade: C');
         }
 
         else {
             if(marks < 69 && marks >= 60){
-                console.log('D');
+                console.log('Your Grade: D');
             }
 
             else(marks < 59 && marks >= 0);{
-                console.log('F');
+                console.log('Your Grade: F');
             }
         }
     }
