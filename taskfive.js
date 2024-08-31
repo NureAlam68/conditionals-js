84cb16c94d86484f4517b60f82105622f94167b6
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 14:02:30 +0600

    added task five

---
+++ taskfive.js
@@ -0,0 +1,19 @@
+let num1 = 10;
+let num2 = 20;
+let result;
+
+// if(num1 > num2) {
+//     result = num1 * 2;
+//     console.log(result);
+// }
+
+// else {
+//     result = num1 + num2;
+//     console.log(result);
+// }
+
+
+// ternary 1 line
+
+num1 > num2 ?  result = num1 * 2 :  result = num1 + num2;
+console.log(result)
