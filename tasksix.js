61685fc73f6b522b15286992bf15d5d3015e6dc2
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 14:42:08 +0600

    added task six

--- tasksix.js
+++ tasksix.js
@@ -20,3 +20,31 @@
 //     - Students get a 50% discount
 //     - Senior citizens (age >= 60) gets a 15% Discount
 //     - Otherwise Regular ticket fare 800 tk
+
+
+
+
+let age = 47;
+let isStudent = false;
+let price = 800;
+
+if (age < 10) {
+    price = 'Free';
+    console.log(price);
+}
+
+else if (isStudent === true) {
+    discount = price * 50 / 100;
+    payAmount = price - discount;
+    console.log(payAmount);
+}
+
+else if (age >= 60) {
+    discount = price * 15 / 100;
+    payAmount = price - discount;
+    console.log(payAmount);
+}
+
+else {
+    console.log(price);
+}
