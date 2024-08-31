a3b3066962d52c85efa2f1637a38dab2108b5264
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 14:17:20 +0600

    added question

--- taskone.js
+++ taskone.js
@@ -1,3 +1,13 @@
+// Free Drinks
+//     - Burger more than 500tk: free Coke
+//     - Else Coke: 30tk
+
+
+
+
+
+
+
 let burgerPrice = 600;
 
 if(burgerPrice > 500){
---
+++ tasksix.js
@@ -0,0 +1,22 @@
+// let price = 500;
+// const isLeader = true;
+
+// // if(isLeader === false) {
+// //     price = 0;
+// // }
+
+// // else {
+// //     price = price + 100;
+// // }
+
+// price = isLeader === true ? 0 : price + 100;
+
+// console.log(price);
+
+
+
+// Ticket fare Calculator
+//     - Children (age < 10): free
+//     - Students get a 50% discount
+//     - Senior citizens (age >= 60) gets a 15% Discount
+//     - Otherwise Regular ticket fare 800 tk
